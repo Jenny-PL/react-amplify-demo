1f6c0e13dceb81103fe00d5c65f5df27e2cf58a2
Jenny <jfperham@gmail.com>
2022-08-24 09:45:34 -0700

    changes to react basic app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={
+            "https://images.unsplash.com/photo-1660811136764-c4c0ded63dc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+          }
+          alt="pic of plants"
+        />
+        <h1>Hello from Jenny!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
